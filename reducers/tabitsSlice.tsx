214f4890feddec7605620aef0df1d76fa8377a51
Yesh Chandiramani <yesh0907@hotmail.com>
2022-01-16 03:25:03 -0800

    Add tabits redux logic

---
+++ reducers/tabitsSlice.tsx
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface Tabit {
+    name: string,
+    freq: number[],
+    streak: number,
+    end: Date | undefined
+}
+
+const initialState: Tabit[] = [
+    { 
+        name: "Read 10 minutes in the morning", 
+        freq: [0, 2, 4],
+        streak: 4,
+        end: new Date("2022-02-14")
+    },
+    {
+        name: "Walk 10,000 steps",
+        freq: [1,5],
+        streak: 20,
+        end: undefined
+    },
+    {
+        name: "Call Mom",
+        freq: [6],
+        streak: 2,
+        end: new Date("2022-06-30")
+    }
+];
+
+export const tabitsSlice = createSlice({
+    name: 'tabits',
+    initialState,
+    reducers: {
+        addTabit: (state, action) => {
+            state.push(action.payload);
+        },
+        updateTabitStreak: (state, action) => {
+            const { name } = action.payload;
+            const existingTabit = state.find(tabit => tabit.name === name);
+            if (existingTabit) {
+                existingTabit.streak += 1;
+            }
+        }
+    }
+});
+
+export const { addTabit, updateTabitStreak } = tabitsSlice.actions;
+
+export default tabitsSlice.reducer;
--- store.tsx
+++ store.tsx
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import userReducer from './reducers/userSlice';
+import tabitsReducer from './reducers/tabitsSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        tabits: tabitsReducer,
     }
 });
 
